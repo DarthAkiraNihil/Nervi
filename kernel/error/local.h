7241da24f2568cb85b3bdac731b0ce228ce5e5c8
TheSwagVader <egorzverev666@gmail.com>
2023-06-10 12:27:42 +0300

    Added some exceptions and improved doc

--- kernel/error/local.h
+++ kernel/error/local.h
@@ -6,28 +6,51 @@
 #ifndef NERVI_LOCAL_H
 #define NERVI_LOCAL_H
 
-class InvalidIndexException: public std::exception {
-private:
-    std::string message_;
-public:
-    explicit InvalidIndexException(const std::string& message);
-    const char* what() const noexcept override {
-        return message_.c_str();
-    }
-};
+namespace NerviInternalExceptions {
 
-class LockedAdressException: public std::exception {
-private:
-    std::string message_;
-public:
-    explicit LockedAdressException(const std::string& message);
-    const char* what() const noexcept override {
-        return message_.c_str();
-    }
-};
+    class InvalidIndexException : public std::exception {
+    private:
+        std::string message_;
+    public:
+        explicit InvalidIndexException(const std::string &message);
 
+        const char *what() const noexcept override { return message_.c_str(); }
+    };
 
-LockedAdressException::LockedAdressException(const std::string& message) : message_(message) {}
-InvalidIndexException::InvalidIndexException(const std::string& message) : message_(message) {}
+    class LockedAddressException : public std::exception {
+    private:
+        std::string message_;
+    public:
+        explicit LockedAddressException(const std::string &message);
 
+        const char *what() const noexcept override { return message_.c_str(); }
+    };
+
+    class InvalidRegisterException : public std::exception {
+    private:
+        std::string message_;
+    public:
+        explicit InvalidRegisterException(const std::string &message);
+
+        const char *what() const noexcept override { return message_.c_str(); }
+    };
+
+    class InstructionPointerInterruptionPushException : public std::exception {
+    private:
+        std::string message_;
+    public:
+        explicit InstructionPointerInterruptionPushException(const std::string &message);
+
+        const char *what() const noexcept override { return message_.c_str(); }
+    };
+
+    InstructionPointerInterruptionPushException::InstructionPointerInterruptionPushException(const std::string &message) : message_(message) {}
+
+    LockedAddressException::LockedAddressException(const std::string &message) : message_(message) {}
+
+    InvalidIndexException::InvalidIndexException(const std::string &message) : message_(message) {}
+
+    InvalidRegisterException::InvalidRegisterException(const std::string &message) : message_(message) {}
+
+}
 #endif //NERVI_LOCAL_H
--- kernel/storage/machinememory.h
+++ kernel/storage/machinememory.h
@@ -1,3 +1,9 @@
+/**
+ * \file machinememory.h
+ * \brief TEST TEST TEST
+ */
+
+
 #include <memory.h>
 #include <list>
 #include <kernel/error/local.h>
@@ -41,108 +47,134 @@ namespace Nervi {
     };
 
     bool NVirtualMachineStorage::isLocked(long long index) {
-        for (auto iterator = this->locked.cbegin(); iterator != this->locked.cend(); iterator++) {
-            if (*iterator == index) {
-                return true;
-            }
+        if (index < 0 || index > this->size - 1) {
+            throw NerviInternalExceptions::InvalidIndexException(fmt::format("Invalid required index: {} (expected positive and less than {})", index, this->size));
         }
-        return false;
-        //todo error if no cell found
+        else {
+            for (auto iterator = this->locked.cbegin(); iterator != this->locked.cend(); iterator++) {
+                if (*iterator == index) {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 
-/**
- * \brief The NVirtualMachineStorage constructor that creates a null-determined storage device.
- * Initializes storage's memory array with the defined size and writes to its cells zeros
- * \param size The size of storage array in bytes. Max is 9 223 372 036 854 775 807 bytes
- */
+    /**
+     * \brief The NVirtualMachineStorage constructor that creates a null-determined storage device.
+     * Initializes storage's memory array with the defined size and initializes its cells with zeros
+     * \param size The size of storage array in bytes. Max is 2^64 - 1 bytes
+     */
     NVirtualMachineStorage::NVirtualMachineStorage(long long size) {
         this->size = size;
         this->storage = new char[size];
         memset(this->storage, 0, size);
     }
 
-/**
- * \brief The NVirtualMachineStorage destructor that released all its used resources.
- * Deletes all storage array data that have been reserved by creating an object of the class, clears the list of the locked addresses and defines its size as 0
- */
+    /**
+     * \brief The NVirtualMachineStorage destructor that released all its used resources.
+     * Deletes all storage array data that have been reserved by creating an object of the class, clears the list of the locked addresses and defines its size as 0
+     */
     NVirtualMachineStorage::~NVirtualMachineStorage() {
         delete[] this->storage;
         this->size = 0;
         this->locked.clear();
     }
 
-/**
- * \brief Sets a storage cell's status as write-locked. It pushes back the index to the list of the locked
- * @param index The address of a lockable cell
- */
+    /**
+     * \brief Sets a storage cell's status as write-locked. It pushes back the index to the list of the locked
+     * \param index The address of a cell to lock
+     * \throw InvalidIndexException If the index is out of bounds of the storage array
+     */
     void NVirtualMachineStorage::lockCell(long long index) {
-        this->locked.push_back(index);
-        //todo no action if locked
+        if (index < 0 || index > this->size - 1) {
+            throw NerviInternalExceptions::InvalidIndexException(fmt::format("Invalid required index to block: {} (expected positive and less than {})", index, this->size));
+        }
+        else {
+            this->locked.push_back(index);
+        }
     }
 
-/**
- * \brief Unlocks a storage cell from write-protection. It erases the unlockable adress from the list of the locked
- * @param index The address of an unlockable cell
- */
+    /**
+     * \brief Unlocks a storage cell from write-protection
+     * \details Unlocks a storage cell from write-protection. It erases the cell to unlock from the list of the locked,
+     * but if the required cell is not locked nothing happens
+     * \param index The address of an cell to unlock
+     * \throw InvalidIndexException If the index is out of bounds of the storage array
+     */
     void NVirtualMachineStorage::unlockCell(long long index) {
-        //todo no action if not locked
-        for (auto iterator = this->locked.cbegin(); iterator != this->locked.cend(); iterator++) {
-            if (*iterator == index) {
-                this->locked.erase(iterator);
-                break;
+        if (index < 0 || index > this->size - 1) {
+            throw NerviInternalExceptions::InvalidIndexException(fmt::format("Invalid required index to block: {} (expected positive and less than {})", index, this->size));
+        }
+        else {
+            for (auto iterator = this->locked.cbegin(); iterator != this->locked.cend(); iterator++) {
+                if (*iterator == index) {
+                    this->locked.erase(iterator);
+                    break;
+                }
             }
         }
     }
 
-/**
- * Returns the size of the storage
- * @return The size of the storage
- */
+    /**
+     * Returns the size of the storage
+     * @return The size of the storage
+     */
     long long NVirtualMachineStorage::getSize() {
         return this->size;
     }
 
-/**
- * Writes a value to the cell of the storage at desired index.
- * @param index The adress of destination
- * \param value The value to write
- * \throw InvalidIndexException If the index is out of bounds of the storage array
- * \throw LockedAdressException If selected cell is write-locked
- */
+    /**
+     * Writes a value to the cell of the storage at desired index.
+     * \param index The address of destination
+     * \param value The value to write
+     * \throw InvalidIndexException If the index is out of bounds of the storage array
+     * \throw LockedAddressException If selected cell is write-locked
+     */
     void NVirtualMachineStorage::setValueAt(long long index, char value) {
         if (index < 0 || index > this->size) {
-            throw InvalidIndexException(fmt::format("Invalid required index: {} (expected positive and less than {})", index, this->size));
+            throw NerviInternalExceptions::InvalidIndexException(fmt::format("Invalid required index: {} (expected positive and less than {})", index, this->size));
         } else if (!(this->isLocked(index))) {
             this->storage[index] = value;
         } else {
-            //std::runtime_error("The required adress is write-locked");
-            throw LockedAdressException(fmt::format("Memory cell with the address {} is write-locked!", index));
+            //std::runtime_error("The required address is write-locked");
+            throw NerviInternalExceptions::LockedAddressException(fmt::format("Memory cell with the address {} is write-locked!", index));
         }
     }
 
-/**
- * Returns a value of a cell of the storage at desired index
- * @param index The address of a cell to get value
- * @return The value of selected cell
- * \throw InvalidIndexException If the index is out of bounds of the storage array
- */
+    /**
+     * Returns a value of a cell of the storage at desired index
+     * @param index The address of a cell to get value
+     * @return The value of selected cell
+     * \throw InvalidIndexException If the index is out of bounds of the storage array
+     */
     char NVirtualMachineStorage::getValueAt(long long index) {
         if (index < 0 || index > this->size) {
-            throw InvalidIndexException(fmt::format("Invalid required index: {} (expected positive and less than {})", index, this->size));
+            throw NerviInternalExceptions::InvalidIndexException(fmt::format("Invalid required index: {} (expected positive and less than {})", index, this->size));
         } else {
             return this->storage[index];
         }
     }
 
+    /**
+     * \brief Pushes a value to a memory register
+     * \details Assigns the passed value to the proper field of NRegisters structure.
+     * If LASTINTR has been passed then NRegisters.LASTINTR will be chosen, else NRegisters.CHAR_REGS[<passed register's name>]
+     * \warning Do not try to push a value to IP because its type is long long, not char, else
+     * InstructionPointerInterruptionPushException will be called
+     * \param registerName The name of the register to push to
+     * \param value A value to push
+     * \throw InstructionPointerInterruptionPushException If there has been an attempt to push a value to IP register
+     * \throw InvalidRegisterException If the required register does not exist
+     */
     void NVirtualMachineStorage::pushToRegister(Nervi::NRegisterNames registerName, char value) {
         if (registerName > 27) {
-            //throw
+            throw NerviInternalExceptions::InvalidRegisterException(fmt::format("Invalid required register index: {}", int(registerName)));
         }
         else {
             switch (registerName) {
                 case IP: {
-                    //throw...
-                    break;
+                    throw NerviInternalExceptions::InstructionPointerInterruptionPushException("Trying to push a value into IP with the method pushToRegister has no sense due the difference between char and long long types");
                 }
                 case LASTINTR: {
                     this->registers.LASTINTR = value;

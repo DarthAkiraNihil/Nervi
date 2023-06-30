f86f7bbc4aeb7859eff3ad623389b90e2741cdba
TheSwagVader <egorzverev666@gmail.com>
2023-06-30 08:15:28 +0300

    Put NCommand structures into namespace NerviKernel

--- kernel/command/ncommand.h
+++ kernel/command/ncommand.h
@@ -5,32 +5,32 @@
 #ifndef NERVI_NCOMMAND_H
 #define NERVI_NCOMMAND_H
 
-
+namespace NerviKernel {
 /**
  * \brief An advanced structure to store a memory address
  * \details Represents a structure that stores an advanced memory address with disc number and the and address in the disc
  */
-struct NMemoryAddress {
-    short discNumber;
-    long long address;
-};
+    struct NMemoryAddress {
+        short discNumber;
+        long long address;
+    };
 
 /**
  * \brief A structure to store Nervi command arguments
  * \details The structure can store an advanced memory address or a char value
  */
-struct NCommandArgument {
-    NMemoryAddress argAddress;
-    char argData;
-};
+    struct NCommandArgument {
+        NMemoryAddress argAddress;
+        char argData;
+    };
 
 /**
  * \brief A structure to represent a Nervi command
  * \details Stores the number of a command presented by its plugin index and its own index (number in the plugin's command list) and two NCommandArguments
  */
-struct NCommand {
-    int pluginIndex, commandIndex;
-    NCommandArgument fArg, sArg;
-};
-
+    struct NCommand {
+        int pluginIndex, commandIndex;
+        NCommandArgument fArg, sArg;
+    };
+}
 #endif //NERVI_NCOMMAND_H

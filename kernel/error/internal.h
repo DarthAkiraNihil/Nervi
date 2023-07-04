1fd2a7e698fa9d0f11c4569199eb3753b532c184
TheSwagVader <egorzverev666@gmail.com>
2023-07-04 12:02:27 +0300

    Documented all internal exceptions

--- kernel/error/internal.h
+++ kernel/error/internal.h
@@ -8,6 +8,14 @@
 
 namespace NerviInternalExceptions {
 
+
+    /**
+    * \brief Represents the class of the exception caused by addressing an invalid cell
+    * \details This is the class of the exception that is thrown if a cell with an invalid index has been addressed.
+    * The invalid indexes are any below zero or any greater than the size of a memory device (i.e. NMemoryCard or NVirtualMachineStorage)
+    * These methods throw the exception:
+    * todo doc these
+    */
     class InvalidIndexException : public std::exception {
     private:
         std::string message_;
@@ -17,6 +25,11 @@ namespace NerviInternalExceptions {
         const char *what() const noexcept override { return message_.c_str(); }
     };
 
+    /**
+    * \brief Represents the class of the exception caused by addressing a locked cell
+    * \details This is the class of the exception that is thrown if a locked cell has been addressed.
+    * The throwing happens when you try to set a value of a cell that you have been locked before the setting.
+    */
     class LockedAddressException : public std::exception {
     private:
         std::string message_;
@@ -26,6 +39,12 @@ namespace NerviInternalExceptions {
         const char *what() const noexcept override { return message_.c_str(); }
     };
 
+    /**
+    * \brief Represents the class of the exception caused by addressing an non-existent register
+    * \details This is the class of the exception that is thrown if the number of register you are trying to push a value into represent an unknown register
+    * The number of registers is declared in the enumeration NerviKernel::NRegisterNames and the indexes of them are between 0 and 28 includely
+    * \warning The exception is not thrown if you are trying to push a value into the IP
+    */
     class InvalidRegisterException : public std::exception {
     private:
         std::string message_;
@@ -35,6 +54,11 @@ namespace NerviInternalExceptions {
         const char *what() const noexcept override { return message_.c_str(); }
     };
 
+    /**
+    * \brief Represents the class of the exception caused by addressing the IP in the methods pushToRegister and getRegister
+    * \details This is the class of the exception that is thrown if you are trying to address the IP in the methods pushToRegister and getRegister.
+     *This has no sense due the difference between char and long long types, use getIP, jump, jumpNext or returnJump instead
+    */
     class InstructionPointerInterruptionPushException : public std::exception {
     private:
         std::string message_;
@@ -43,6 +67,12 @@ namespace NerviInternalExceptions {
 
         const char *what() const noexcept override { return message_.c_str(); }
     };
+
+    /**
+    * \brief Represents the class of the exception that is used only for testing
+    * \details This is the class of the exception that is thrown in any situation that is needed by a tester of a developer.
+    * \warning Do not use this exception in the kernel headers because this exception is not for general functionality, just for testing
+    */
     class DeveloperTestException : public std::exception {
     private:
         std::string message_;
